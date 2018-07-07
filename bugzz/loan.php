022ad5116411afa853b2617efc791650080083b4
Nikita Agarwalla <nikitaagarwalla758@gmail.com>
2018-07-08 01:38:01 +0530

    updating bugzz

---
+++ bugzz/loan.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title>
+            
+        </title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		
+        <style>
+            .box{
+                height: 300px;
+                width:500px;
+            }
+			 body{
+                 background-image: url(images/bid.png);
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-attachment: fixed;
+            }
+
+        </style>
+		
+    </head>
+    <body>
+        
+        
+     <div class="w3-card w3-container w3-content w3-display-middle box w3-hover-orange w3-hover-opacity">
+        <center>
+        <div class="w3-display-middle">
+		<form class="loan1">
+         <b class=""> ENTER YOUR BHAMASHAH ID</b>
+         <input type="number" id="bid" placeholder="Enter your BHAMASHAH ID" required class="w3-margin w3-hover-yellow"/><br/>
+		  <b class=""> UPLOAD CERTIFICATE XEROX</b>
+        <input type="file" style="border:none;border-bottom:2px solid orange;background-color:inherit;color:black;" name="txnmobile" placeholder="Upload your skill certificate" required class="w3-padding-16 w3-input w3-border w3-round w3-content"/><br/>
+         <br/>
+		 
+         <input type="submit" id="btnapply" value="Apply" class="w3-btn w3-ripple w3-hover-red w3-margin-top"><a href="trainer_user.php"></a></input>
+          </form>
+		</div>
+         </center>
+     </div>
+        
+        
+        
+        
+        
+    </body>
+</html>
---
+++ bugzz/update_login.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>
+            
+        </title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <style>
+            .box{
+                height: 200px;
+                width:500px;
+            }
+			 body{
+                 background-image: url(images/update.png);
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-attachment: fixed;
+            }
+
+        </style>
+    </head>
+    <body>
+        
+        
+     <div class="w3-card w3-container w3-content w3-display-middle box w3-hover-orange w3-hover-opacity" >
+        <center>
+        <div class="w3-display-middle">
+		<form class="loan">
+         <b class=""> ENTER YOUR BHAMASHAH ID</b>
+         <input type="number" id="bid" placeholder="Enter your BHAMASHAH ID" required class="w3-margin w3-hover-yellow"/>
+         <br/>
+         <input type="submit" value="Apply" id="btnapply" class="w3-btn w3-ripple w3-hover-red w3-margin-top"><a href="update.php"></a></input>
+		 </form>
+         </div>
+         </center>
+     </div>
+        
+        
+        
+        
+        
+    </body>
+</html>
